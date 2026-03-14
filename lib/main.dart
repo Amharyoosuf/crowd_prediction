3cfe03c07ba0612375a1fc6d3ccd88783415b32c
Amharyoosuf <Amhar12356@gmail.com>
2026-03-14 14:08:13 +0530

    authentification screeen created

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'widgets/bottom_nav.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://piaxgaebpjcrxscgkuuz.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpYXhnYWVicGpjcnhzY2drdXV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0NzA2NTEsImV4cCI6MjA4OTA0NjY1MX0.obgQ9iyEXH7ENJqJTESgflvITW6Y8IlWd8QaPC-gKQw',
+  );
+
   runApp(const CrowdPredictApp());
 }
 
